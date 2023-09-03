9a72942d532c0f6d7200cd62f5a31ced5043cbf1
Eduardo Trintin <duh.trintin@gmail.com>
2023-09-03 18:03:05 -0300

    Ultimos ajustes

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -21,66 +21,66 @@ select club,
   ) t;
 
 --top defensores
-select club, player_name, sum(pt_br) + sum(pt_tk) + sum(pt_tw) +  sum(pt_fo) + sum(pt_rd) + sum(pt_yw) as resultado
-  from (
-        select dc.club, dc.player_name, cast(0.5 * df.balls_recoverd as float) as pt_br, cast(0.5 * df.tackles as float) as pt_tk, df.t_won as pt_tw, cast(-0.5 * dc.fouls_committed as float) as pt_fo, (-2 * dc.red) as pt_rd, (-1 * dc.yellow) as pt_yw
-          from defending df 
-        join disciplinary dc on dc.player_name = df.player_name
-        where df.position in ('Midfielder', 'Defender')
-          and df.match_played > 5
+SELECT club, player_name, sum(pt_br) + sum(pt_tk) + sum(pt_tw) +  sum(pt_fo) + sum(pt_rd) + sum(pt_yw) AS resultado
+  FROM (
+        SELECT dc.club, dc.player_name, cast(0.5 * df.balls_recoverd AS FLOAT) AS pt_br, cast(0.5 * df.tackles AS FLOAT) AS pt_tk, df.t_won AS pt_tw, cast(-0.5 * dc.fouls_committed AS FLOAT) AS pt_fo, (-2 * dc.red) AS pt_rd, (-1 * dc.yellow) AS pt_yw
+          FROM defending df 
+          JOIN disciplinary dc ON dc.player_name = df.player_name
+         WHERE df.position IN ('Midfielder', 'Defender')
+           AND df.match_played > 5
   ) t
-group by club, player_name
-order by resultado desc;
+GROUP BY club, player_name
+ORDER BY resultado DESC;
 
 --top defensores global
-with dados_top_def as (
-select club, player_name, sum(pt_br) + sum(pt_tk) + sum(pt_tw) +  sum(pt_fo) + sum(pt_rd) + sum(pt_yw) as resultado
-  from (
-        select dc.club, dc.player_name, cast(0.5 * df.balls_recoverd as float) as pt_br, cast(0.5 * df.tackles as float) as pt_tk, df.t_won as pt_tw, cast(-0.5 * dc.fouls_committed as float) as pt_fo, (-2 * dc.red) as pt_rd, (-1 * dc.yellow) as pt_yw
-          from defending df 
-        join disciplinary dc on dc.player_name = df.player_name
-        where df.position in ('Midfielder', 'Defender')
-          and df.match_played > 5
+WITH dados_top_def AS (
+SELECT club, player_name, sum(pt_br) + sum(pt_tk) + sum(pt_tw) +  sum(pt_fo) + sum(pt_rd) + sum(pt_yw) AS resultado
+  FROM (
+        SELECT dc.club, dc.player_name, cast(0.5 * df.balls_recoverd AS FLOAT) AS pt_br, cast(0.5 * df.tackles AS FLOAT) AS pt_tk, df.t_won AS pt_tw, cast(-0.5 * dc.fouls_committed AS FLOAT) as pt_fo, (-2 * dc.red) AS pt_rd, (-1 * dc.yellow) AS pt_yw
+          FROM defending df 
+          JOIN disciplinary dc ON dc.player_name = df.player_name
+         WHERE df.position IN ('Midfielder', 'Defender')
+           AND df.match_played > 5
   ) t
-group by club, player_name)
-select club, sum(resultado) as resultado
-  from dados_top_def
-group by club
-order by resultado desc;
+GROUP BY club, player_name)
+SELECT club, sum(resultado) as resultado
+  FROM dados_top_def
+GROUP BY club
+ORDER BY resultado DESC;
 
 --top atacantes
-select club, 
+SELECT club, 
        player_name,
-       sum(pt_ass) + sum(pt_off) + sum(pt_att) + sum(pt_ont) + sum(pt_gol) as resultado 
-  from (
-        select atk.club, atk.player_name, (2 * atk.assists) as pt_ass, cast(-0.5 * atk.offsides as float) as pt_off, cast(0.5 * att.total_attempts as float) as pt_att, att.on_target as pt_ont, (3 * g.goals) as pt_gol
-        from attacking atk 
-        join attempts att on att.player_name = atk.player_name 
-        join goals g on g.player_name = att.player_name
-       where atk.position in ('Midfielder', 'Forward')
-         and atk.match_played > 5
+       sum(pt_ass) + sum(pt_off) + sum(pt_att) + sum(pt_ont) + sum(pt_gol) AS resultado 
+  FROM (
+        SELECT atk.club, atk.player_name, (2 * atk.assists) AS pt_ass, cast(-0.5 * atk.offsides AS FLOAT) AS pt_off, cast(0.5 * att.total_attempts AS FLOAT) AS pt_att, att.on_target AS pt_ont, (3 * g.goals) AS pt_gol
+          FROM attacking atk 
+          JOIN attempts att on att.player_name = atk.player_name 
+          JOIN goals g on g.player_name = att.player_name
+         WHERE atk.position in ('Midfielder', 'Forward')
+           AND atk.match_played > 5
   ) t
-group by club, player_name
-order by resultado desc;
+GROUP BY club, player_name
+ORDER BY resultado DESC;
 
 --top atacantes global
-with dados_top_atk as (
-    select club, 
+WITH dados_top_atk AS (
+    SELECT club, 
         player_name,
-        sum(pt_ass) + sum(pt_off) + sum(pt_att) + sum(pt_ont) + sum(pt_gol) as resultado 
-    from (
-            select atk.club, atk.player_name, (2 * atk.assists) as pt_ass, cast(-0.5 * atk.offsides as float) as pt_off, cast(0.5 * att.total_attempts as float) as pt_att, att.on_target as pt_ont, (3 * g.goals) as pt_gol
-            from attacking atk 
-            join attempts att on att.player_name = atk.player_name 
-            join goals g on g.player_name = att.player_name
-        where atk.position in ('Midfielder', 'Forward')
-            and atk.match_played > 5
+        sum(pt_ass) + sum(pt_off) + sum(pt_att) + sum(pt_ont) + sum(pt_gol) AS resultado 
+    FROM (
+          SELECT atk.club, atk.player_name, (2 * atk.assists) AS pt_ass, cast(-0.5 * atk.offsides AS FLOAT) AS pt_off, cast(0.5 * att.total_attempts AS FLOAT) as pt_att, att.on_target AS pt_ont, (3 * g.goals) AS pt_gol
+            FROM attacking atk 
+            JOIN attempts att ON att.player_name = atk.player_name 
+            JOIN goals g ON g.player_name = att.player_name
+           WHERE atk.position IN ('Midfielder', 'Forward')
+             AND atk.match_played > 5
     ) t
-    group by club, player_name)
-select club, sum(resultado) as resultado
-  from dados_top_atk 
-group by club 
-order by resultado desc;
+    GROUP BY club, player_name)
+SELECT club, sum(resultado) AS resultado
+  FROM dados_top_atk 
+GROUP BY club 
+ORDER BY resultado DESC;
 
   
 
--- script_consultas.sql
+++ script_consultas.sql
@@ -18,3 +18,62 @@ SELECT att.club,
   JOIN top_5 tp ON tp.player_name = att.player_name
 ORDER BY mean_goals desc;
 
+--2) Top defensores
+SELECT club, player_name, sum(pt_br) + sum(pt_tk) + sum(pt_tw) +  sum(pt_fo) + sum(pt_rd) + sum(pt_yw) AS resultado
+  FROM (
+        SELECT dc.club, dc.player_name, cast(0.5 * df.balls_recoverd AS FLOAT) AS pt_br, cast(0.5 * df.tackles AS FLOAT) AS pt_tk, df.t_won AS pt_tw, cast(-0.5 * dc.fouls_committed AS FLOAT) AS pt_fo, (-2 * dc.red) AS pt_rd, (-1 * dc.yellow) AS pt_yw
+          FROM defending df 
+          JOIN disciplinary dc ON dc.player_name = df.player_name
+         WHERE df.position IN ('Midfielder', 'Defender')
+           AND df.match_played > 5
+  ) t
+GROUP BY club, player_name
+ORDER BY resultado DESC;
+
+WITH dados_top_def AS (
+SELECT club, player_name, sum(pt_br) + sum(pt_tk) + sum(pt_tw) +  sum(pt_fo) + sum(pt_rd) + sum(pt_yw) AS resultado
+  FROM (
+        SELECT dc.club, dc.player_name, cast(0.5 * df.balls_recoverd AS FLOAT) AS pt_br, cast(0.5 * df.tackles AS FLOAT) AS pt_tk, df.t_won AS pt_tw, cast(-0.5 * dc.fouls_committed AS FLOAT) as pt_fo, (-2 * dc.red) AS pt_rd, (-1 * dc.yellow) AS pt_yw
+          FROM defending df 
+          JOIN disciplinary dc ON dc.player_name = df.player_name
+         WHERE df.position IN ('Midfielder', 'Defender')
+           AND df.match_played > 5
+  ) t
+GROUP BY club, player_name)
+SELECT club, sum(resultado) as resultado
+  FROM dados_top_def
+GROUP BY club
+ORDER BY resultado DESC;
+
+--3) Top atacantes
+SELECT club, 
+       player_name,
+       sum(pt_ass) + sum(pt_off) + sum(pt_att) + sum(pt_ont) + sum(pt_gol) AS resultado 
+  FROM (
+        SELECT atk.club, atk.player_name, (2 * atk.assists) AS pt_ass, cast(-0.5 * atk.offsides AS FLOAT) AS pt_off, cast(0.5 * att.total_attempts AS FLOAT) AS pt_att, att.on_target AS pt_ont, (3 * g.goals) AS pt_gol
+          FROM attacking atk 
+          JOIN attempts att on att.player_name = atk.player_name 
+          JOIN goals g on g.player_name = att.player_name
+         WHERE atk.position in ('Midfielder', 'Forward')
+           AND atk.match_played > 5
+  ) t
+GROUP BY club, player_name
+ORDER BY resultado DESC;
+
+WITH dados_top_atk AS (
+    SELECT club, 
+        player_name,
+        sum(pt_ass) + sum(pt_off) + sum(pt_att) + sum(pt_ont) + sum(pt_gol) AS resultado 
+    FROM (
+          SELECT atk.club, atk.player_name, (2 * atk.assists) AS pt_ass, cast(-0.5 * atk.offsides AS FLOAT) AS pt_off, cast(0.5 * att.total_attempts AS FLOAT) as pt_att, att.on_target AS pt_ont, (3 * g.goals) AS pt_gol
+            FROM attacking atk 
+            JOIN attempts att ON att.player_name = atk.player_name 
+            JOIN goals g ON g.player_name = att.player_name
+           WHERE atk.position IN ('Midfielder', 'Forward')
+             AND atk.match_played > 5
+    ) t
+    GROUP BY club, player_name)
+SELECT club, sum(resultado) AS resultado
+  FROM dados_top_atk 
+GROUP BY club 
+ORDER BY resultado DESC;
